fad768aa8dc7cd073f927e3baec49d92a4167708
Karsten Schnitter <k.schnitter@sap.com>
2024-02-16 17:45:21 +0100

    Migrate to Spring Boot 3 and Java 17
    Signed-off-by: Karsten Schnitter <k.schnitter@sap.com>

--- sample-spring-boot/src/main/java/com/sap/hcp/cf/logging/sample/springboot/keystore/KeyStoreDynLogConfiguration.java
+++ sample-spring-boot/src/main/java/com/sap/hcp/cf/logging/sample/springboot/keystore/KeyStoreDynLogConfiguration.java
@@ -1,39 +1,37 @@
 package com.sap.hcp.cf.logging.sample.springboot.keystore;
 
-import java.security.interfaces.RSAPublicKey;
-
+import com.sap.hcp.cf.logging.servlet.dynlog.DynamicLogLevelConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.sap.hcp.cf.logging.servlet.dynlog.DynamicLogLevelConfiguration;
+import java.security.interfaces.RSAPublicKey;
 
 /**
- * Provides the public key for JWT validation out of the keystore.
- * Otherwise adheres to the configuration by environment variables.
- *
+ * Provides the public key for JWT validation out of the keystore. Otherwise adheres to the configuration by environment
+ * variables.
  */
 @Component
 public class KeyStoreDynLogConfiguration implements DynamicLogLevelConfiguration {
 
-	private TokenKeyProvider keyProvider;
-	private String dynLogHeaderKey;
+    private TokenKeyProvider keyProvider;
+    private String dynLogHeaderKey;
 
-	public KeyStoreDynLogConfiguration(@Autowired TokenKeyProvider keyProvider,
-			@Value("${DYN_LOG_HEADER:SAP-LOG-LEVEL}") String dynLogHeaderKey) {
-		this.keyProvider = keyProvider;
-		this.dynLogHeaderKey = dynLogHeaderKey;
-	}
+    public KeyStoreDynLogConfiguration(@Autowired TokenKeyProvider keyProvider,
+                                       @Value("${DYN_LOG_HEADER:SAP-LOG-LEVEL}") String dynLogHeaderKey) {
+        this.keyProvider = keyProvider;
+        this.dynLogHeaderKey = dynLogHeaderKey;
+    }
 
-	@Override
-	public String getDynLogHeaderKey() {
-		return dynLogHeaderKey;
-	}
+    @Override
+    public String getDynLogHeaderKey() {
+        return dynLogHeaderKey;
+    }
 
-	@Override
-	public RSAPublicKey getRsaPublicKey() {
-		String keyId = keyProvider.getPrivateKeyId();
-		return keyProvider.getPublicKeyById(keyId);
-	}
+    @Override
+    public RSAPublicKey getRsaPublicKey() {
+        String keyId = keyProvider.getPrivateKeyId();
+        return keyProvider.getPublicKeyById(keyId);
+    }
 
 }
