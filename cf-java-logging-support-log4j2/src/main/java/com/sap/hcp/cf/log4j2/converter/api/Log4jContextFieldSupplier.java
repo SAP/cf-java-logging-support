3a9f016a4e8030743ae7fccfac523b62fc925b6b
Karsten Schnitter <k.schnitter@sap.com>
2025-02-27 09:52:31 +0100

    Add comment on order constants for Field Suppliers
    Signed-off-by: Karsten Schnitter <k.schnitter@sap.com>

--- cf-java-logging-support-log4j2/src/main/java/com/sap/hcp/cf/log4j2/converter/api/Log4jContextFieldSupplier.java
+++ cf-java-logging-support-log4j2/src/main/java/com/sap/hcp/cf/log4j2/converter/api/Log4jContextFieldSupplier.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.core.LogEvent;
 @FunctionalInterface
 public interface Log4jContextFieldSupplier extends EventContextFieldSupplier<LogEvent> {
 
+    // These are constants for the order. The default is 0. To retain the original order, negative values were chosen.
     int BASE_FIELDS = -100;
     int CONTEXT_FIELDS = -90;
     int REQUEST_FIELDS = -80;
--- cf-java-logging-support-logback/src/main/java/com/sap/hcp/cf/logback/converter/api/LogbackContextFieldSupplier.java
+++ cf-java-logging-support-logback/src/main/java/com/sap/hcp/cf/logback/converter/api/LogbackContextFieldSupplier.java
@@ -6,6 +6,7 @@ import com.sap.hcp.cf.logging.common.serialization.EventContextFieldSupplier;
 @FunctionalInterface
 public interface LogbackContextFieldSupplier extends EventContextFieldSupplier<ILoggingEvent> {
 
+    // These are constants for the order. The default is 0. To retain the original order, negative values were chosen.
     int BASE_FIELDS = -100;
     int CONTEXT_FIELDS = -90;
     int REQUEST_FIELDS = -80;
