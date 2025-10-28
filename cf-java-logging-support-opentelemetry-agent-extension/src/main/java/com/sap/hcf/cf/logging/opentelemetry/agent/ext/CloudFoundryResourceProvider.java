b5ec46fce06b55d18c7ec894dcad2c758675f476
Karsten Schnitter <k.schnitter@sap.com>
2025-10-28 14:45:14 +0100

    Adjust to OpenTelemetry Java Contrib 1.50.0
    There was a breaking change making the CloudFoundryResourceProvider
    final. Therefore inheritance needed to be replaced with delegation. The new
    implementation uses a common lazy initialization pattern to ensure the
    correct instance is used.
    
    Signed-off-by: Karsten Schnitter <k.schnitter@sap.com>

--- cf-java-logging-support-opentelemetry-agent-extension/src/main/java/com/sap/hcf/cf/logging/opentelemetry/agent/ext/CloudFoundryResourceProvider.java
+++ cf-java-logging-support-opentelemetry-agent-extension/src/main/java/com/sap/hcf/cf/logging/opentelemetry/agent/ext/CloudFoundryResourceProvider.java
@@ -2,16 +2,36 @@ package com.sap.hcf.cf.logging.opentelemetry.agent.ext;
 
 import com.sap.hcf.cf.logging.opentelemetry.agent.ext.attributes.CloudFoundryResourceCustomizer;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
 import io.opentelemetry.sdk.resources.Resource;
 
-public class CloudFoundryResourceProvider
-        extends io.opentelemetry.contrib.cloudfoundry.resources.CloudFoundryResourceProvider {
+import java.util.ServiceLoader;
+
+public class CloudFoundryResourceProvider implements ResourceProvider {
 
     private final CloudFoundryResourceCustomizer customizer = new CloudFoundryResourceCustomizer();
 
     @Override
     public Resource createResource(ConfigProperties configProperties) {
-        Resource original = super.createResource(configProperties);
-        return customizer.apply(original, configProperties);
+        ResourceProvider delegate = getDelegate();
+        return delegate == null
+                ? Resource.empty()
+                : customizer.apply(delegate.createResource(configProperties), configProperties);
+    }
+
+    private ResourceProvider getDelegate() {
+        return DelegateHolder.INSTANCE;
+    }
+
+    private static class DelegateHolder {
+        static final ResourceProvider INSTANCE = loadCloudFoundryResourceProvider();
+
+        private static ResourceProvider loadCloudFoundryResourceProvider() {
+            ServiceLoader<ResourceProvider> loader = ServiceLoader.load(ResourceProvider.class);
+            return loader.stream().map(ServiceLoader.Provider::get)
+                         .filter(p -> p instanceof io.opentelemetry.contrib.cloudfoundry.resources.CloudFoundryResourceProvider)
+                         .findAny().orElse(null);
+        }
+
     }
 }
