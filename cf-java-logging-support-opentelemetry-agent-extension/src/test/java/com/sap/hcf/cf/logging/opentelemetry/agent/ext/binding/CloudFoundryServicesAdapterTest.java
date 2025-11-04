1f0afb935db56d7347b15824e82f955d05e02762
Karsten Schnitter <k.schnitter@sap.com>
2025-11-04 09:37:03 +0100

    Use correct Java 11 build for OTel Extension
    Switch to <release> tag with compiler plugin.
    This forces Java 11 compliant code requiring a test change.
    
    Signed-off-by: Karsten Schnitter <k.schnitter@sap.com>

--- cf-java-logging-support-opentelemetry-agent-extension/src/test/java/com/sap/hcf/cf/logging/opentelemetry/agent/ext/binding/CloudFoundryServicesAdapterTest.java
+++ cf-java-logging-support-opentelemetry-agent-extension/src/test/java/com/sap/hcf/cf/logging/opentelemetry/agent/ext/binding/CloudFoundryServicesAdapterTest.java
@@ -4,71 +4,70 @@ import org.assertj.core.api.AbstractListAssert;
 import org.assertj.core.api.ObjectAssert;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CloudFoundryServicesAdapterTest {
-    private static final String DEFAULT_VCAP_SERVICES = """
-            {
-              "managed-find-me-service": [
-                {
-                  "label": "managed-find-me-service",
-                  "tags": ["Find Me!"],
-                  "name": "managed-find-me1"
-                },
-                {
-                  "label": "managed-find-me-service",
-                  "tags": ["Find Me!"],
-                  "name": "managed-find-me2"
-                },
-                {
-                  "label": "managed-find-me-service",
-                  "tags": ["You can't see me!"],
-                  "name": "managed-other"
-                }
-              ],
-              "managed-notice-me-not-service": [
-                {
-                  "label": "managed-notice-me-not-service",
-                  "tags": ["Find Me!"],
-                  "name": "managed-other1"
-                },
-                {
-                  "label": "managed-notice-me-not-service",
-                  "tags": ["You can't see me!"],
-                  "name": "managed-other2"
-                }
-              ],
-              "user-provided": [
-                {
-                  "label": "user-provided",
-                  "tags": ["Find Me!"],
-                  "name": "ups-find-me1"
-                },
-                {
-                  "label": "user-provided",
-                  "tags": ["Find Me!"],
-                  "name": "ups-find-me2"
-                },
-                {
-                  "label": "user-provided",
-                  "tags": ["You can't see me!"],
-                  "name": "ups-other"
-                }
-              ]
-            }""";
+    private static final String DEFAULT_VCAP_SERVICES = "{\n" + //
+            "  \"managed-find-me-service\": [\n" + //
+            "    {\n" + //
+            "      \"label\": \"managed-find-me-service\",\n" + //
+            "      \"tags\": [\"Find Me!\"],\n" + //
+            "      \"name\": \"managed-find-me1\"\n" + //
+            "    },\n" + //
+            "    {\n" + //
+            "      \"label\": \"managed-find-me-service\",\n" + //
+            "      \"tags\": [\"Find Me!\"],\n" + //
+            "      \"name\": \"managed-find-me2\"\n" + //
+            "    },\n" + //
+            "    {\n" + //
+            "      \"label\": \"managed-find-me-service\",\n" + //
+            "      \"tags\": [\"You can't see me!\"],\n" + //
+            "      \"name\": \"managed-other\"\n" + //
+            "    }\n" + //
+            "  ],\n" + //
+            "  \"managed-notice-me-not-service\": [\n" + //
+            "    {\n" + //
+            "      \"label\": \"managed-notice-me-not-service\",\n" + //
+            "      \"tags\": [\"Find Me!\"],\n" + //
+            "      \"name\": \"managed-other1\"\n" + //
+            "    },\n" + //
+            "    {\n" + //
+            "      \"label\": \"managed-notice-me-not-service\",\n" + //
+            "      \"tags\": [\"You can't see me!\"],\n" + //
+            "      \"name\": \"managed-other2\"\n" + //
+            "    }\n" + //
+            "  ],\n" + //
+            "  \"user-provided\": [\n" + //
+            "    {\n" + //
+            "      \"label\": \"user-provided\",\n" + //
+            "      \"tags\": [\"Find Me!\"],\n" + //
+            "      \"name\": \"ups-find-me1\"\n" + //
+            "    },\n" + //
+            "    {\n" + //
+            "      \"label\": \"user-provided\",\n" + //
+            "      \"tags\": [\"Find Me!\"],\n" + //
+            "      \"name\": \"ups-find-me2\"\n" + //
+            "    },\n" + //
+            "    {\n" + //
+            "      \"label\": \"user-provided\",\n" + //
+            "      \"tags\": [\"You can't see me!\"],\n" + //
+            "      \"name\": \"ups-other\"\n" + //
+            "    }\n" + //
+            "  ]\n" + //
+            "}";
 
     private static final CloudFoundryServicesAdapter DEFAULT_ADAPTER =
             new CloudFoundryServicesAdapter(DEFAULT_VCAP_SERVICES);
 
     @Test
     void getsAllServicesWithNullParameters() {
-        List<CloudFoundryServiceInstance> services = DEFAULT_ADAPTER.stream(null, null).toList();
+        List<CloudFoundryServiceInstance> services = DEFAULT_ADAPTER.stream(null, null).collect(toList());
         assertServiceNames(services).containsExactly("managed-find-me1", "managed-find-me2", "managed-other",
                                                      "managed-other1", "managed-other2", "ups-find-me1", "ups-find-me2",
                                                      "ups-other");
@@ -83,14 +82,15 @@ public class CloudFoundryServicesAdapterTest {
     @Test
     void filtersBySingleLabel() {
         List<CloudFoundryServiceInstance> services =
-                DEFAULT_ADAPTER.stream(Collections.singletonList("managed-find-me-service"), emptyList()).toList();
+                DEFAULT_ADAPTER.stream(singletonList("managed-find-me-service"), emptyList()).collect(toList());
         assertServiceNames(services).containsExactlyInAnyOrder("managed-find-me1", "managed-find-me2", "managed-other");
     }
 
     @Test
     void priotizesByServiceLabel() {
         List<CloudFoundryServiceInstance> services =
-                DEFAULT_ADAPTER.stream(asList("user-provided", "managed-find-me-service"), emptyList()).toList();
+                DEFAULT_ADAPTER.stream(asList("user-provided", "managed-find-me-service"), emptyList())
+                               .collect(toList());
         assertServiceNames(services).containsExactly("ups-find-me1", "ups-find-me2", "ups-other", "managed-find-me1",
                                                      "managed-find-me2", "managed-other");
     }
@@ -98,7 +98,7 @@ public class CloudFoundryServicesAdapterTest {
     @Test
     void filtersBySingleTag() {
         List<CloudFoundryServiceInstance> services =
-                DEFAULT_ADAPTER.stream(emptyList(), Collections.singletonList("Find Me!")).toList();
+                DEFAULT_ADAPTER.stream(emptyList(), singletonList("Find Me!")).collect(toList());
         assertServiceNames(services).containsExactlyInAnyOrder("managed-find-me1", "managed-find-me2", "managed-other1",
                                                                "ups-find-me1", "ups-find-me2");
     }
@@ -106,8 +106,8 @@ public class CloudFoundryServicesAdapterTest {
     @Test
     void standardUseCase() {
         List<CloudFoundryServiceInstance> services =
-                DEFAULT_ADAPTER.stream(asList("user-provided", "managed-find-me-service"),
-                                       Collections.singletonList("Find Me!")).collect(toList());
+                DEFAULT_ADAPTER.stream(asList("user-provided", "managed-find-me-service"), singletonList("Find Me!"))
+                               .collect(toList());
         assertServiceNames(services).containsExactly("ups-find-me1", "ups-find-me2", "managed-find-me1",
                                                      "managed-find-me2");
     }
