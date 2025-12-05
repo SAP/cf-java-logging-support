ba52f1851a131cee24f620da44e4757a62b0bd8d
Karsten Schnitter <k.schnitter@sap.com>
2025-12-05 09:57:46 +0100

    Create Final Release 4.0.1
    Signed-off-by: Karsten Schnitter <k.schnitter@sap.com>

--- cf-java-logging-support-opentelemetry-agent-extension/src/main/java/com/sap/hcf/cf/logging/opentelemetry/agent/ext/CloudLoggingConfigurationCustomizerProvider.java
+++ cf-java-logging-support-opentelemetry-agent-extension/src/main/java/com/sap/hcf/cf/logging/opentelemetry/agent/ext/CloudLoggingConfigurationCustomizerProvider.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 public class CloudLoggingConfigurationCustomizerProvider implements AutoConfigurationCustomizerProvider {
 
     private static final Logger LOG = Logger.getLogger(CloudLoggingConfigurationCustomizerProvider.class.getName());
-    private static final String VERSION = "4.0.0";
+    private static final String VERSION = "4.0.1";
 
     @Override
     public void customize(AutoConfigurationCustomizer autoConfiguration) {
